4c7b758b5a75b18b2e3b35c01ae7b00551a5a33a
nwiedel <164350832+nwiedel@users.noreply.github.com>
2026-06-15 23:30:39 +0200

    1.5. Player bewegen

--- core/src/main/java/de/nicolas/mygame/screens/GameScreen.java
+++ core/src/main/java/de/nicolas/mygame/screens/GameScreen.java
@@ -1,10 +1,14 @@
 package de.nicolas.mygame.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 import de.nicolas.mygame.MyGame;
+import de.nicolas.mygame.utils.GameConfig;
 
 public class GameScreen implements Screen {
 
@@ -17,6 +21,8 @@ public class GameScreen implements Screen {
     private float playerY;
     private float playerWidth;
     private float playerHeight;
+    private float playerSpeed;
+    private Vector2 movement;
 
     public GameScreen(MyGame game){
         this.game = game;
@@ -25,13 +31,15 @@ public class GameScreen implements Screen {
     @Override
     public void show() {
         // Aufruf einmalig zu Beginn
-
         batch = new SpriteBatch();
         playerTexture = new Texture("player.png");
+
         playerX = 100;
         playerY = 100;
         playerWidth = 128;
         playerHeight = 128;
+        playerSpeed = 220;
+        movement = new Vector2(0, 0);
     }
 
     @Override
@@ -39,6 +47,65 @@ public class GameScreen implements Screen {
         // Aufruf in jedem Frame
         update(delta);
         draw();
+    }
+
+    private void update(float delta){
+        // Spielelogik
+        handleInput(delta);
+        keepPlayerInsideScreen();
+    }
+
+    private void handleInput(float delta){
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) ||
+            Gdx.input.isKeyPressed(Input.Keys.D)){
+            movement.x += 1;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) ||
+            Gdx.input.isKeyPressed(Input.Keys.A)){
+            movement.x -= 1;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP) ||
+            Gdx.input.isKeyPressed(Input.Keys.W)){
+            movement.y += 1;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN) ||
+            Gdx.input.isKeyPressed(Input.Keys.S)){
+            movement.y -= 1;
+        }
+
+        if (movement.len2() > 0){
+            movement.nor();
+
+            playerX += movement.x * playerSpeed * delta;
+            playerY += movement.y * playerSpeed * delta;
+        }
+
+        movement.set(0, 0);
+    }
+
+    private void keepPlayerInsideScreen(){
+        if (playerX < 0){
+            playerX = 0;
+        }
+        if (playerY < 0){
+            playerY = 0;
+        }
+        if (playerX + playerWidth > Gdx.graphics.getWidth()){
+            playerX = Gdx.graphics.getWidth() - playerWidth;
+        }
+        if (playerY + playerHeight > Gdx.graphics.getHeight()){
+            playerY = Gdx.graphics.getHeight() - playerHeight;
+        }
+    }
+
+    private void draw(){
+        // das Zeichnen
+
+        ScreenUtils.clear(GameConfig.CORNFLOWER_BLUE);
+
+        batch.begin();
+        batch.draw(playerTexture, playerX, playerY, playerWidth, playerHeight);
+        batch.end();
     }
 
     @Override
@@ -66,21 +133,5 @@ public class GameScreen implements Screen {
         // säubert überflüssige Ressourcen
         batch.dispose();
         playerTexture.dispose();
-    }
-
-    // == private Methoden ==
-    private void update(float delta){
-        // Spielelogik
-        playerX += 50 * delta;
-    }
-
-    private void draw(){
-        // das Zeichnen
-
-        ScreenUtils.clear(0.1f, 0.1f, 0.15f, 1f);
-
-        batch.begin();
-        batch.draw(playerTexture, playerX, playerY, playerWidth, playerHeight);
-        batch.end();
     }
 }
