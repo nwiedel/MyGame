e9ebb085e4aa0ee707b9849be36369c2d76a98e5
nicolaswiedel <nicolaswiedel@googlemail.com>
2026-06-15 17:46:49 +0200

    1.4.1. Korrektur des Bildes

--- core/src/main/java/de/nicolas/mygame/screens/GameScreen.java
+++ core/src/main/java/de/nicolas/mygame/screens/GameScreen.java
@@ -1,12 +1,22 @@
 package de.nicolas.mygame.screens;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import de.nicolas.mygame.MyGame;
 
 public class GameScreen implements Screen {
 
-    private MyGame game;
+    private final MyGame game;
+
+    private SpriteBatch batch;
+    private Texture playerTexture;
+
+    private float playerX;
+    private float playerY;
+    private float playerWidth;
+    private float playerHeight;
 
     public GameScreen(MyGame game){
         this.game = game;
@@ -15,6 +25,13 @@ public class GameScreen implements Screen {
     @Override
     public void show() {
         // Aufruf einmalig zu Beginn
+
+        batch = new SpriteBatch();
+        playerTexture = new Texture("player.png");
+        playerX = 100;
+        playerY = 100;
+        playerWidth = 128;
+        playerHeight = 128;
     }
 
     @Override
@@ -47,16 +64,23 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
         // säubert überflüssige Ressourcen
+        batch.dispose();
+        playerTexture.dispose();
     }
 
     // == private Methoden ==
     private void update(float delta){
         // Spielelogik
+        playerX += 50 * delta;
     }
 
     private void draw(){
         // das Zeichnen
 
         ScreenUtils.clear(0.1f, 0.1f, 0.15f, 1f);
+
+        batch.begin();
+        batch.draw(playerTexture, playerX, playerY, playerWidth, playerHeight);
+        batch.end();
     }
 }
