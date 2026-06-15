216cc97223ca3e853f59493513fb9de7c195f0cf
nicolaswiedel <nicolaswiedel@googlemail.com>
2026-06-15 11:58:36 +0200

    1.2.2 Die Game Loop

--- core/src/main/java/de/nicolas/mygame/MyGame.java
+++ core/src/main/java/de/nicolas/mygame/MyGame.java
@@ -1,6 +1,7 @@
 package de.nicolas.mygame;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -9,20 +10,46 @@ import de.nicolas.mygame.utils.GameConfig;
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class MyGame extends ApplicationAdapter {
 
+    private float playerX;
+    private float playerY;
+    private float playerSpeed;
+
     @Override
     public void create() {
-        System.out.println("Spiel gestartet!");
+        playerX = 100f;
+        playerY = 100f;
+        playerSpeed = 120f;
     }
+
+
 
     @Override
     public void render() {
-        ScreenUtils.clear(GameConfig.CORNFLOWER_BLUE);
+        float delta = Gdx.graphics.getDeltaTime();
 
-        System.out.println("Frame gezeichent!");
+        handleInput(delta);
+        update(delta);
+        draw();
+    }
+
+    private void handleInput(float delta){
+        // Tastatur, mouse, Touch, Gamepad
+    }
+
+    private void update(float delta){
+        //Bewegung, Kollision, Timer und Spiellogik
+
+        playerX += playerSpeed * delta;
+    }
+
+    private void draw(){
+        // Zeichnen
+        
+        ScreenUtils.clear(GameConfig.CORNFLOWER_BLUE);
     }
 
     @Override
     public void dispose() {
-        System.out.println("Spiel beendet!");
+
     }
 }
